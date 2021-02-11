93f9707f79d97a72c134f5de466cec88d51021cc
Manten Devillé <manten.d@hotmail.com>
2021-02-11 15:52:18 +0100

    Add test model

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Test;
 
 class FormController extends Controller
 {
     public function loadFormView(){
         return view('form');
     }
+    //store data in database through eloquent
+    public function store(){
+        request()->validate([
+            'name'=> ['required']
+            ]);
+        $test = new Test
+    }
+
 }
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -7,8 +7,12 @@
 <form action="" method="post">
 
 
-<input type="text"> vraag 1 </input>
-<input type="text"> vraag 2 </input>
+<input type="text" name="name"> vraag 1 </input>
+<input type="text" name="surname"> vraag 2 </input>
+<input type="text" name="email"> email </input>
+
+<input type="text" name="adress"> vraag 2 </input>
+
 </form>
 
 @endsection
